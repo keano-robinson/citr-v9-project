c337b6174c2b247b77a5103315de7938280a09ec
Keano Robinson <keano.robinson@hotmail.com>
2025-11-12 17:44:26 +0000

    Add prettier

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,36 +1,36 @@
 const Pizza = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", { key: "name" }, props.name),
-    React.createElement("p", { key: "description" }, props.description),
-  ]);
+	return React.createElement("div", {}, [
+		React.createElement("h2", { key: "name" }, props.name),
+		React.createElement("p", { key: "description" }, props.description),
+	]);
 };
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement(
-      "h1",
-      { style: { color: "red" } },
-      "Hello, Padre Gino's!"
-    ),
-    React.createElement(Pizza, {
-      name: "Margherita",
-      description: "Classic pizza with tomatoes, mozzarella, and basil.",
-    }),
-    React.createElement(Pizza, {
-      name: "Pepperoni",
-      description: "Topped with pepperoni and cheese.",
-    }),
-    React.createElement(Pizza, {
-      name: "Veggie",
-      description: "Loaded with fresh vegetables.",
-    }),
-    React.createElement(Pizza, {
-      name: "Hawaiian",
-      description: "Ham and pineapple topping.",
-    })
-  );
+	return React.createElement(
+		"div",
+		{},
+		React.createElement(
+			"h1",
+			{ style: { color: "red" } },
+			"Hello, Padre Gino's!",
+		),
+		React.createElement(Pizza, {
+			name: "Margherita",
+			description: "Classic pizza with tomatoes, mozzarella, and basil.",
+		}),
+		React.createElement(Pizza, {
+			name: "Pepperoni",
+			description: "Topped with pepperoni and cheese.",
+		}),
+		React.createElement(Pizza, {
+			name: "Veggie",
+			description: "Loaded with fresh vegetables.",
+		}),
+		React.createElement(Pizza, {
+			name: "Hawaiian",
+			description: "Ham and pineapple topping.",
+		}),
+	);
 };
 
 const container = document.getElementById("root");
