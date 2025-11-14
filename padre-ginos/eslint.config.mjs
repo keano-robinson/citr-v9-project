a9da4d72fa7d6803545a5863b8dfbef202411c57
Keano Robinson <keano.robinson@hotmail.com>
2025-11-14 12:57:22 +0000

    Extend eslint config to handle React

--- padre-ginos/eslint.config.mjs
+++ padre-ginos/eslint.config.mjs
@@ -1,13 +1,23 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 // below comment is a trick to get proper type checking and autocompletion in IDEs for this config file
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	js.configs.recommended,
 	{
-		files: ["**/*.js"],
+		...reactPlugin.configs.flat.recommended,
+		settings: {
+			react: {
+				version: "detect",
+			},
+		},
+	},
+	reactPlugin.configs.flat.jsxRuntime,
+	{
+		files: ["**/*.js", "**/*.jsx"],
 		languageOptions: {
 			globals: { ...globals.browser, ...globals.node },
 			parserOptions: {
@@ -15,6 +25,11 @@ export default [
 					jsx: true,
 				},
 			},
+		},
+		rules: {
+			// your custom rules here
+			"react/no-unescaped-entities": "off",
+			"react/prop-types": "off",
 		},
 	},
 	prettier,
